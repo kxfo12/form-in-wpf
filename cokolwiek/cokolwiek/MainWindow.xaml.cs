5948297bfee0aee7040a4472ff94977738f2c0b3
kxfo <147588035+kxfo12@users.noreply.github.com>
2024-11-21 13:41:31 +0100

    Add files via upload

--- cokolwiek/cokolwiek/MainWindow.xaml.cs
+++ cokolwiek/cokolwiek/MainWindow.xaml.cs
@@ -20,9 +20,28 @@ namespace cokolwiek
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string Powitanie { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            Powitanie = "Hi";
+            DataContext = this;
+        }
+
+        private void Change_Text_Button(object sender, RoutedEventArgs e)
+        {
+            powitanie.Content = "The text has been changed!";
+            MessageBox.Show("Do you like the change?", "Very not important question", MessageBoxButton.YesNo, MessageBoxImage.Question);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int number1 = 0, number2 = 0;
+            if(int.TryParse(firstNumber.Text, out number1) && int.TryParse(secondNumber.Text, out number2))
+            {
+                int sum = number1 + number2;
+                MessageBox.Show("Suma: " + sum);
+            }
         }
     }
 }
